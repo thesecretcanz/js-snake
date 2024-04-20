96a05077ca7b46033571ed2934888124fcee5156
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2024-04-20 17:32:48 +0000

    Better classes example

--- examples/classes-examples.js
+++ examples/classes-examples.js
@@ -1,4 +1,18 @@
 document.write('\
 \
-<h1>This is an example of classes</h1>\
+\
+<h1 id="header"></h1>\
+\
+<script>\
+class Car: {\
+    constructor(name, year) {\
+        this.name = name;\
+        this.year = year;\
+    }\
+}\
+const car = new Car("Fiat", "2003");\
+document.getElementById("header").innerHTML = car.name + " Panda " + car.year;\
+</script>\
+\
+\
 ');
