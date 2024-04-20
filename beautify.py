46c2e090a826eb5feac2dbf8cadac87c9ebad1a1
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2024-04-20 18:43:54 +0000

    Fixed examples error and beautify to automatically generate external js files

---
+++ beautify.py
@@ -0,0 +1,38 @@
+file = open("beautify/file.html", "r")
+fileContent = file.read()
+file.close()
+
+def insert(array, index, element):
+    newArray = array[:index]
+    newArray.append(element)
+    newArray = newArray + array[index:]
+    return newArray
+
+charLst = []
+
+for letter in fileContent:
+	charLst.append(letter)
+
+index = 0
+counter = 0
+while index < len(charLst):
+    if ord(charLst[index]) == 10:
+        counter += 1
+    index = index + 1
+
+
+index = 0
+specialCharCounter = 0
+while index < len(charLst):
+    if ord(charLst[index]) == 10 and specialCharCounter <= counter:
+        charLst = insert(charLst, index, chr(92))
+        specialCharCounter = specialCharCounter + 1
+        index = index + 1
+    index = index + 1
+
+jsFileContent = "".join(str(element) for element in charLst)
+jsFileContent = "document.write('\\" + "\n" + jsFileContent + "');"
+
+file = open("beautify/file.js", "w")
+file.write(jsFileContent)
+file.close()
