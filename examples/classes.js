a21dfc315bbf5d637c1a5e6b57e5f49ab5bf018c
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2024-04-20 19:28:51 +0000

    Trying to fix preview

--- examples/classes.js
+++ examples/classes.js
@@ -1,1 +1,16 @@
-document.write('<h1>SOS</h1>');
+document.write('\
+\
+<h1 id="header"></h1>\
+\
+<script>\
+    class Car {\
+        constructor(carName, carYear) {\
+            this.name = carName;\
+            this.year = carYear;\
+        }\
+    }\
+    myCar = Car("Fiat", "2003")\
+    document.getElementById("header").innerHTML = myCar.name;\
+</script>\
+\
+');
