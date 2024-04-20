126869251bc63677b38799eab5407fc97e174115
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2024-04-20 22:19:15 +0000

    Small adjustments

--- js/examples-list.js
+++ js/examples-list.js
@@ -1,6 +1,6 @@
 document.write('\
 \
-<h2>EXAMPLES LINKS</h2>\
+<h2 style="color:white">EXAMPLES LINKS</h2>\
 \
 <p><a href="../html/examples/classes.html" style="color:white">Classes example</a></p>\
 <p><a href="../html/examples/button.html" style="color:white">Button example</a></p>\
