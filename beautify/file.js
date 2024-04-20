26a1f519d18cf135ab5819fea083ffbb31b12385
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2024-04-20 18:53:43 +0000

    Trying to fix preview

--- beautify/file.js
+++ beautify/file.js
@@ -1,12 +1,3 @@
 document.write('\
-<h1 id="header"></h1>\
-<script>\
-    class Car {\
-        constructor(name, year) {\
-            this.name = name;\
-            this.year = year;\
-        }\
-        myCar = new Car("Fiat", "2003");\
-    }\
-    document.getElementById("header").innerHTML = myCar.name;\
-</script>');
+<h1>SOS</h1>\
+');
