67a50f45570a1a49368e882b1a5ddcbebd01bf9d
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2024-04-20 19:36:29 +0000

    Trying to fix preview

--- examples/example.js
+++ examples/example.js
@@ -3,13 +3,6 @@ document.write('\
 <h1 id="header"></h1>\
 \
 <script>\
-    class Car {\
-        constructor(carName, carYear) {\
-            this.name = carName;\
-            this.year = carYear;\
-        }\
-    }\
-    myCar = Car("Fiat", "2003")\
     document.getElementById("header").innerHTML = "SOS";\
 </script>\
 \
