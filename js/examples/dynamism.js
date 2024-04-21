4506659ab01684cf76b64f55cfc555b7beadd6c3
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2024-04-21 11:14:09 +0000

    Working on dynamism example

--- js/examples/dynamism.js
+++ js/examples/dynamism.js
@@ -2,5 +2,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-console.log('Latest update 13:06');
-sleep(2000).then(() => { console.log('Done!'); });
+console.log('Latest update 13:13')
+console.log('Hello');
+sleep(2000).then(() => { console.log('World!'); });
