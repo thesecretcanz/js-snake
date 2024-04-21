04078900f0a440c71fc20402f11c5a2e0a4536dc
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2024-04-21 09:28:44 +0000

    Still working on dynamism example

--- js/embedded-html/examples-list.js
+++ js/embedded-html/examples-list.js
@@ -3,7 +3,7 @@ document.write('\
 <h2 style="color:white">Examples list</h2>\
 <p>\
     <a href="html/examples/classes.html" style="color:white">Classes example</a><br>\
-    <a href="html/examples/button.html" style="color:white">Button example</a>\
+    <a href="html/examples/button.html" style="color:white">Button example</a><br>\
     <a href="html/examples/dynamism.html" style="color:white">Dynamism example</a>\
 </p>\
 \
--- js/examples/dynamism.js
+++ js/examples/dynamism.js
@@ -1,4 +1,4 @@
-setTimeout(run(), 1000);
+setTimeout(run(), 5000);
             
 function run() {
     document.getElementById("text").innerHTML = "done";
