c6929bfdb9f0c5575e6849d3b9dd3f55e3e445af
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2024-04-21 08:11:59 +0000

    Trying to fix example links

--- js/examples-list.js
+++ js/examples-list.js
@@ -2,8 +2,8 @@ document.write('\
 \
 <h2 style="color:white">Examples list</h2>\
 <p>\
-    <a href="./html/examples/classes.html" style="color:white">Classes example</a><br>\
-    <a href="/html/examples/button.html" style="color:white">Button example</a>\
+    <a href="html/examples/classes.html" style="color:white">Classes example</a><br>\
+    <a href="html/examples/button.html" style="color:white">Button example</a>\
 </p>\
 \
 ');
