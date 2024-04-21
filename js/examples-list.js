7e04ce7e712e2cb70e1bb7413190177b23d9a6f5
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2024-04-21 07:59:33 +0000

    Better example links

--- js/examples-list.js
+++ js/examples-list.js
@@ -1,8 +1,9 @@
 document.write('\
 \
-<h2 style="color:white">EXAMPLES LINKS</h2>\
-\
-<p><a href="../html/examples/classes.html" style="color:white">Classes example</a></p>\
-<p><a href="../html/examples/button.html" style="color:white">Button example</a></p>\
+<h2 style="color:white">Examples list</h2>\
+<p>\
+    <a href="../html/examples/classes.html" style="color:white">Classes example</a><br>\
+    <a href="../html/examples/button.html" style="color:white">Button example</a>\
+</p>\
 \
 ');
